e5cef96754db5e2c2a1fc06825cfd94fc1f31e13
SCPassion <bernard.cryptohk@gmail.com>
2025-06-23 21:28:38 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function HomePage() {
+import Guest from "@/components/Guest";
+import AddTransaction from "@/components/AddTransaction";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
   return (
     <main>
-      <h1>Expense Tracker</h1>
+      <h1>Welcome, {user.firstName}</h1>
+      <AddTransaction />
     </main>
   );
 }
---
+++ components/AddTransaction.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { addTransaction } from "@/actions/addTransaction";
+
+export default function AddTransaction() {
+  async function clientAction(formData: FormData) {
+    const { data, error } = await addTransaction(formData);
+
+    if (error) {
+      alert(error);
+    } else {
+      alert("Transaction added");
+      console.log("Transaction data:", data);
+    }
+  }
+
+  return (
+    <>
+      <h3>Add transaction</h3>
+      <form action={clientAction}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            name="text"
+            id="text"
+            required
+            placeholder="Enter text..."
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br /> (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            step="0.01"
+            placeholder="Enter amount..."
+            required
+          />
+        </div>
+        <button type="submit" className="btn">
+          Add transaction
+        </button>
+      </form>
+    </>
+  );
+}
